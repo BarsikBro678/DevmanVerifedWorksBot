fe83ba12a3ac2f6622e1a165d94fca89f1a9938e
BarsikBro678 <112183697+BarsikBro678@users.noreply.github.com>
2023-07-30 15:09:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,39 +5,44 @@ import telegram
 
 
 def get_verifed_work(devman_api_key, timestamp=None):
-	url = "https://dvmn.org/api/long_polling/"
-	if timestamp:
-		url = f"{url}?timestamp={timestamp}"
-	headers = {
-		"Authorization": f"Token {devman_api_key}"
-	}
-	response = requests.get(url, headers=headers)
-	response.raise_for_status()
-	return response.json()
+    url = "https://dvmn.org/api/long_polling/"
+    payload = {
+	"timestamp": timestamp,
+    }
+    headers = {
+	"Authorization": f"Token {devman_api_key}"
+    }
+    response = requests.get(url, params=payload, headers=headers)
+    response.raise_for_status()
+    return response.json()
 
 
 def telegram_bot(bot, chat_id, devman_api_key):
-	timestamp = None
-	while True:
-		try:
-			work = get_verifed_work(devman_api_key, timestamp=timestamp)
+    timestamp = None
+    while True:
+	try:
+	    work = get_verifed_work(devman_api_key, timestamp=timestamp)
 				
-			if work["status"] == "timeout":
-				timestamp = my_work["timestamp_to_request"]
-			else:
-				timestamp = None
-				if work["new_attempts"][0]["is_negative"]:
-					bot.send_message(chat_id=chat_id, text=f"Преподаватель оценил вашу работу <<{my_work['new_attempts'][0]['lesson_title']}>>!	                                                                К сожалению, в работе нашлись ошибки.")
-				else:
-					bot.send_message(chat_id=chat_id, text=f"Преподаватель оценил вашу работу <<{my_work['new_attempts'][0]['lesson_title']}>>!	                                                                Преподавателю все понравилось, можно приступать к следуещему уроку!")
+	    if work["status"] == "timeout":
+		timestamp = my_work["timestamp_to_request"]
+	    else:
+		timestamp = None
+		if work["new_attempts"][0]["is_negative"]:
+		    bot.send_message(chat_id=chat_id, text=f"Преподаватель оценил вашу работу <<{my_work['new_attempts'][0]['lesson_title']}>>!	                                                                К сожалению, в работе нашлись ошибки.")
+		else:
+		    bot.send_message(chat_id=chat_id, text=f"Преподаватель оценил вашу работу <<{my_work['new_attempts'][0]['lesson_title']}>>!	                                                                Преподавателю все понравилось, можно приступать к следуещему уроку!")
 
-		except requests.exceptions.ReadTimeout or ConnectionError:
-			pass
+	except requests.exceptions.ReadTimeout or ConnectionError:
+	    pass
 
 	
 def main():
-	chat_id = os.environ["MY_CHAT_ID"]
-	telegram_bot_key = os.environ["TELEGRAM_BOT_KEY"]
-	devman_api_key = os.environ["DEVMAN_API_KEY"]
-	bot = telegram.Bot(token=telegram_bot_key)
-	telegram_bot(bot, chat_id, devman_api_key)
+    chat_id = os.environ["MY_CHAT_ID"]
+    telegram_bot_key = os.environ["TELEGRAM_BOT_KEY"]
+    devman_api_key = os.environ["DEVMAN_API_KEY"]
+    bot = telegram.Bot(token=telegram_bot_key)
+    telegram_bot(bot, chat_id, devman_api_key)
+
+
+if __name__ == "__main__":
+    main()
