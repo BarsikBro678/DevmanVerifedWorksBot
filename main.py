4208558d97ac2f75c63b41510c3a98ad9cb56f6a
BarsikBro678 <112183697+BarsikBro678@users.noreply.github.com>
2023-07-30 14:16:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import telegram
 
 
-def get_my_verifed_work(devman_api_key, timestamp=None):
+def get_verifed_work(devman_api_key, timestamp=None):
 	url = "https://dvmn.org/api/long_polling/"
 	if timestamp:
 		url = f"{url}?timestamp={timestamp}"
@@ -22,7 +22,7 @@ def get_my_verifed_work(devman_api_key, timestamp=None):
 def telegram_bot(bot, chat_id, devman_api_key):
 	timestamp = None
 	while True:
-		my_work = get_my_verifed_work(devman_api_key, timestamp=timestamp)
+		my_work = get_verifed_work(devman_api_key, timestamp=timestamp)
 		if my_work["status"] == "timeout":
 			timestamp = my_work["timestamp_to_request"]
 		else:
@@ -34,8 +34,8 @@ def telegram_bot(bot, chat_id, devman_api_key):
 
 	
 def main():
-	my_chat_id = os.environ["MY_CHAT_ID"]
+	chat_id = os.environ["MY_CHAT_ID"]
 	telegram_bot_key = os.environ["TELEGRAM_BOT_KEY"]
 	devman_api_key = os.environ["DEVMAN_API_KEY"]
 	bot = telegram.Bot(token=telegram_bot_key)
-	telegram_bot(bot, my_chat_id, devman_api_key)
+	telegram_bot(bot, chat_id, devman_api_key)
